fdba460c6b2f84b867d74228bbf3c59688befc61
pavel.nopanic <pavel.nopanic@gmail.com>
2020-06-20 22:17:55 +0300

    footer almost done accept logo and icons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {SharedModule} from "./shared/shared.module";
 import {CoreModule} from "./core/core.module";
+import {FooterComponent} from "./components/footer/footer.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
