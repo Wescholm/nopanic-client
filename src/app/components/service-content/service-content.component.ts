901b11c00fe909ef71a8fec14c81f5f98304ea7b
Nikita <Wescholm@gmail.com>
2020-06-24 21:31:27 +0300

    Refactoring orders.

--- src/app/components/service-content/service-content.component.ts
+++ src/app/components/service-content/service-content.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServiceContentComponent implements OnInit {
 
+  public orders: number[] = [1,2,3,4,5,6,7,8,9]
+
   constructor() { }
 
   ngOnInit(): void {
