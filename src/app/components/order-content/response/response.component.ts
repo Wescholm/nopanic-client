ac31d116177dbdf0ae6ed08152e60b558700e054
pavel.nopanic <pavel.nopanic@gmail.com>
2020-06-29 18:30:39 +0300

    create 2 components response and response-fields

---
+++ src/app/components/order-content/response/response.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'response',
+  templateUrl: './response.component.html',
+  styleUrls: ['./response.component.scss']
+})
+export class ResponseComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {}
+
+}
