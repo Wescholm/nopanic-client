b9ffdff788458fb82cd5b02fffdf87664adffabf
pavel.nopanic <pavel.nopanic@gmail.com>
2020-06-29 22:21:53 +0300

    change names components add-response and order-response

---
+++ src/app/components/order-content/order-response/order-response.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'order-response',
+  templateUrl: './order-response.component.html',
+  styleUrls: ['./order-response.component.scss']
+})
+export class OrderResponseComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {}
+
+}
